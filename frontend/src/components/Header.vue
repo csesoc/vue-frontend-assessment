6201392298337f18d98c7fa9012d49bdaa9855d6
Darian Lee <darianlmj@gmail.com>
2022-08-12 00:16:24 +1000

    feat(frontend): added header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="header-container">
+    <a id="logo">Pogjects</a>
+    <div>
+      Vue is Fun
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .header-container {
+    width: 80vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 1rem;
+    /* background-color: #f5f5f5; */
+    border-bottom: 1px solid #e5e5e5;
+  }
+
+  #logo {
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-decoration: none;
+  }
+</style>
